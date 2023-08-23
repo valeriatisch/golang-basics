0e6411fa8907432e69c2be4a4303d7cf6b842b53
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 21:07:21 +0600

    control flow code skeleton

---
+++ 04_control_flow/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	// For loop: the only one with semicolons
+
+	// TODO: Create slice and for loop through it
+
+	// Range
+
+	// "While" loop
+
+	// Infinite loops
+
+	// If-else
+
+	// Two statements in one line
+
+	// Switch
+
+	// Switch with fallthrough
+	// - fallthrough will execute the next case even if it doesn't match
+
+}
