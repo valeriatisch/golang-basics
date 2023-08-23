f2efe30a025b670228e42603605c664e3d59581b
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 19:50:13 +0600

    code skeleton for intro & data types

---
+++ 02_data_types/main.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+	// --- Variables ---
+	// Go is a statically typed language, but it is able to figure out the type for you
+
+	// Declare a variable & assign a value to it with :=
+	// Btw: The variables we declare must always be used
+
+	// Re-assign the variable
+
+	// Can't assign a value to a variable with a different type
+	// x := "hello"
+	// x = false
+
+	// Declare a variable and assign a value with var
+
+	// Declare multiple variables with a type at once
+
+	// Shadowing
+
+	// Zero value: default value for a variable
+	// int: 0
+	// float: 0.0
+	// string: ""
+	// bool: false
+	// nil: functions, pointers, slices etc.
+
+	// A string is immutable, you can assign a new value but can't change the bytes
+
+	// --- Custom Types ---
+	// Creating your own type
+
+	// Conversion
+
+	// --- Logical Operators ---
+
+	// --- Constants ---
+	// Typed
+
+	// Define multiple constants
+	// Untyped
+
+	// Iota
+}
+
+// Can't do this in outer scope/package-level:
+// x := 1
+// but this is ok:
+var globalVar = 2
