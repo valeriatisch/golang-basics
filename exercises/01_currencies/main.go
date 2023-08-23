9718d7116f99e93217e54cd65d47e7a2a5876a7f
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 21:22:12 +0600

    currencies exercise todos

---
+++ exercises/01_currencies/main.go
@@ -0,0 +1,22 @@
+package main
+
+// 1) TODO: Create type a "Currency"
+
+// 2) TODO: Create constants of type Currency: USD = 1.0, EUR = 0.91, GBP = 0.79
+
+/* 3) TODO: Use following function to convert between currencies
+func convert(amount Currency, from Currency, to Currency) Currency {
+	return (amount / from) * to
+}
+*/
+
+func main() {
+
+	// 4) TODO: Ask the user for the amount, its currency and the target currency as strings
+	// Hint: You can use fmt.Scan(&variable) to read user input
+
+	// 5) TODO: Figure out which currencies are the strings the user entered (source & target)
+
+	// 6) TODO: Convert the amount from source to target currency & print it out
+	// Hint: You can use fmt.Printf("%.2f %s, 0.33, "some string") to print out a float and a string in the correct places
+}
