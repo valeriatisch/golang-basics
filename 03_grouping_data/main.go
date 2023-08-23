c2c4a1d4bb42f5d91a064c4ade4d763664f9220c
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 21:00:19 +0600

    grouping data code skeleton

---
+++ 03_grouping_data/main.go
@@ -0,0 +1,45 @@
+package main
+
+// Structs
+// - A struct is a collection of fields.
+
+// Embedded structs
+// - A struct can contain other structs.
+
+func main() {
+	// Arrays
+	// - An array is a fixed-size collection of elements of the same type, like a fixed parking slot.
+	// - An array is declared by specifying the type and size of the array.
+	// - An array is indexed starting at 0.
+
+	// Length of an array is the number of elements it was declared with.
+
+	// Superstar cousin of arrays
+	// They're like magic dynamic parking lots
+	// Slices
+	// - A slice is a dynamically-sized, flexible view into the elements of an array.
+
+	// Composite literal
+	// x := type{values}
+
+	// Multi-dimensional slices
+	// - A slice can contain any type, including other slices.
+
+	// Maps
+	// - A map is an unordered collection of key-value pairs.
+	// - A map is declared by specifying the type of its key and value.
+	// - A map is indexed by its key.
+
+	// No entry
+
+	// Adding new element
+
+	// Deleting an element
+
+	// Structs
+
+	// Embedded structs
+
+	// Anonymous struct
+
+}
