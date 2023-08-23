370fcf0900ba7fa1b9ac3d77198a291e9232ce8e
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 21:25:13 +0600

    password validator exercise todos

---
+++ exercises/02_password/main.go
@@ -0,0 +1,17 @@
+package main
+
+func ValidatePassword(password string) bool {
+
+	// 1) TODO: Ensure password is at least x characters long
+
+	// 2) TODO: Ensure password contains at least one uppercase letter, one lowercase letter, and one digit
+	// Hint: Use unicode.IsUpper & other functions from the unicode package (https://pkg.go.dev/unicode)
+
+	return true
+}
+
+func main() {
+	// 3) TODO: Prompt the user to input a password
+
+	// 4) TODO: Validate the password
+}
