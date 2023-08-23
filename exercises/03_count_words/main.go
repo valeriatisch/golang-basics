58fc5e39d78ac23190e5b19fd437fe4713146980
Valeria Tisch <valeria.tisch97@gmail.com>
2023-08-23 21:31:11 +0600

    count words exercise todos

---
+++ exercises/03_count_words/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// 1) TODO: Ask the user to enter the file name
+
+	// 2) TODO: Read the file using os.ReadFile()
+
+	// 3) TODO: Convert the file content to a string and split it into words with strings.Fields()
+
+	// 4) TODO: Create a map to store the the words and their number of occurrences
+
+	// 5) TODO: Put the words into the map and increment their count
+	// Don't forget to normalize the words by lowercasing them with strings.ToLower()
+
+	// 6) TODO: Print each word and its count
+
+}
